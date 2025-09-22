31b7951746bd76547e35255cb4694f789180dfbb
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-22 11:21:49 +0300

    feat: add logo images

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,28 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-   
+      <nav className="bg-white shadow-md p-4">
+        <div className="logoImagesContainer flex items-center ">
+          {/* First Logo - Image Logo */}
+          <Image 
+            src="/image-logo.png" 
+            alt="Aya Logo"
+            width={100}
+            height={100}
+
+            
+          />
+
+          {/* Second Logo - Name Logo */}
+          <Image 
+            src="/name-logo.png" 
+            alt="Aya Name"
+            width={70}
+            height={70}
+           
+          />
+        </div>
+      </nav>
     </>
   );
 };
