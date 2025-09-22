8df8694950e570c1d382acf291536665b456e3d7
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-22 11:56:20 +0300

    feat: add Navbar component with logo, navigation links, and contact button

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,27 +4,60 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-white shadow-md p-4">
-        <div className="logoImagesContainer flex items-center ">
-          {/* First Logo - Image Logo */}
-          <Image 
-            src="/image-logo.png" 
-            alt="Aya Logo"
-            width={100}
-            height={100}
+      <nav className="bg-white shadow-md flex">
+        {/* ------------------------------------- logo images -------------------------------------   */}
+        <a>
+          {" "}
+          <div className="logoImagesContainer flex items-center cursor-pointer ">
+            {/* First Logo - Image Logo */}
 
-            
-          />
+            <Image
+              src="/image-logo.png"
+              alt="Aya Logo"
+              width={100}
+              height={100}
+            />
 
-          {/* Second Logo - Name Logo */}
-          <Image 
-            src="/name-logo.png" 
-            alt="Aya Name"
-            width={70}
-            height={70}
-           
-          />
+            {/* Second Logo - Name Logo */}
+            <Image src="/name-logo.png" alt="Aya Name" width={70} height={70} />
+          </div>
+        </a>
+
+        {/* ------------------------------------- Nav Links -------------------------------------   */}
+        <ul>
+          <li>
+            <a href="#top"></a>Home
+          </li>
+          <li>
+            <a href="#about"></a>About
+          </li>
+          <li>
+            <a href="#services"></a>Services
+          </li>
+          <li>
+            <a href="#work"></a>My Work
+          </li>
+          <li>
+            <a href="#contact"></a>Contact me
+          </li>
+        </ul>
+
+        {/* ------------------------------------- Contact Button -------------------------------------   */}
+
+        <div>
+          <a href="">
+            Contact
+            <Image
+              src="/images/icons/arrow-icon.png" 
+              alt="Arrow Icon"
+              width={16}
+              height={16}
+            />
+          </a>
         </div>
+
+
+        
       </nav>
     </>
   );
