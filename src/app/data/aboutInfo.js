ebe18e95b4375ec4ffebdb24541cac58795891df
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-23 19:39:35 +0300

    add user info data file

---
+++ src/app/data/aboutInfo.js
@@ -0,0 +1,23 @@
+// src/data/aboutInfo.js
+import { FaUserTie, FaGraduationCap, FaBriefcase } from "react-icons/fa";
+
+export const aboutInfo = [
+  {
+    icon: <FaUserTie />,
+    title: "Fields",
+    description: `Content Creator, Script Writer, Media Buyer, Social Media Marketer, 
+    Social Media Specialist, Copywriter, Article Writer (SEO Optimized), 
+    Food Photographer, Reel Woman (Reels Production & Editing).`,
+  },
+  {
+    icon: <FaGraduationCap />,
+    title: "Education",
+    description: "Studied Media Department, Faculty of Arts, Sohag University.",
+  },
+  {
+    icon: <FaBriefcase />,
+    title: "Projects",
+    description:
+      "Worked on multiple successful marketing campaigns and content projects.",
+  },
+];
