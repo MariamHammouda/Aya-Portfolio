a199f4686ddde3f9af01709024f1e6ce33481496
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-23 19:34:20 +0300

    feat(about): add about me section details

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -9,7 +9,6 @@ const About = () => {
 
       {/* in this div we will create two columns */}
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-
         <div className="userImageContainer w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
             src={"/images/profile-img.jpg"}
@@ -21,7 +20,23 @@ const About = () => {
         </div>
 
         <div className="flex-1">
-
+          <p className="mb-10 max-w-2xl font-Ovo">
+            I'm a Social Media Specialist and Content Creator passionate about
+            helping brands grow their online presence. With experience in
+            content creation, copywriting, and SEO, I create strategies that not
+            only engage audiences but also drive measurable results. My
+            expertise spans across Social Media Marketing, Ads Management, and
+            Media Buying, covering platforms like Facebook, Instagram, TikTok,
+            YouTube, Google Ads, and more. By leveraging platform algorithms, I
+            help businesses achieve the best ROI from their campaigns. In
+            addition to marketing, I bring skills in script writing, article
+            writing (SEO-friendly), and AI-powered content generation, ensuring
+            every piece of content is both creative and optimized. I also work
+            on visual storytelling through product photography and professional
+            Reels production. I believe in blending strategy, creativity, and
+            technology to build impactful digital identities and long-term brand
+            growth.
+          </p>
         </div>
       </div>
     </div>
