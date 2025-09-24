263e120f248476cbc0d3277da81e2ad7905b8621
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-24 12:46:38 +0300

    feat(services): create services data

---
+++ src/app/data/serviceData.js
@@ -0,0 +1,44 @@
+export const serviceData = [
+  {
+    icon: "/images/icons/content-creator.png",
+    title: "Content Creation",
+    description:
+      "Crafting creative and engaging content tailored to each platform to strengthen brand presence.",
+    link: "#",
+  },
+  {
+    icon: "/images/icons/script.png",
+    title: "Script Writing",
+    description:
+      "Writing impactful scripts for Reels, videos, and podcasts that capture attention and deliver stories.",
+    link: "#",
+  },
+  {
+    icon: "/images/icons/media_buyer.png",
+    title: "Media Buying",
+    description:
+      "Managing ad campaigns across Facebook, Instagram, TikTok, YouTube, Google Ads, and more for best ROI.",
+    link: "#",
+  },
+  {
+    icon: "/images/icons/social-media.png",
+    title: "Social Media Marketing & Management",
+    description:
+      "Building strategies, creating content plans, and managing communities to grow audience engagement.",
+    link: "#",
+  },
+  {
+    icon: "/images/icons/photography.png",
+    title: "Food Photography & Product Shooting",
+    description:
+      "Creating high-quality visuals that highlight products and boost brand storytelling.",
+    link: "#",
+  },
+  {
+    icon: "/images/icons/video.png",
+    title: "Reels & Video Content",
+    description:
+      "Producing engaging Reels and videos that align with trends and drive audience connection.",
+    link: "#",
+  },
+];
