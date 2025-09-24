e8bf4e4df3a9e1a12e0fad4e3cd9d0086593d65b
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-24 09:16:51 +0300

    feat(about):add user data of about me section

---
+++ src/app/data/aboutInfo.js
@@ -0,0 +1,31 @@
+
+export const aboutInfo = [
+  {
+     icon: {
+      iconLight: "/images/icons/work-icon.png",
+      iconDark: "/images/icons/work-icon-dark.png",
+    },
+    title: "Fields",
+    description: `Content Creator, Script Writer, Media Buyer, Social Media Marketer, 
+    Social Media Specialist, Copywriter, Article Writer (SEO Optimized), 
+    Food Photographer, Reel Woman (Reels Production & Editing).`,
+  },
+
+
+  {
+     icon: {
+    iconLight: "/images/icons/edu-icon.png",
+    iconDark: "/images/icons/edu-icon-dark.png",
+  },
+    title: "Education",
+    description: "Studied Media Department, Faculty of Arts, Sohag University.",
+  },
+  
+  {
+  icon: {
+    iconLight: "/images/icons/project_icon.png",
+    iconDark: "/images/icons/project_dark.png",
+  },    title: "Projects",
+    description: "Worked on multiple successful marketing campaigns and content projects.",
+  },
+];
