0dd91169f5b920296e74204c9810666c23839d67
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-09-24 12:13:51 +0300

    feat(services): add services section text

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
+      <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        I help brands grow through creative content, engaging scripts, and
+        impactful ad campaigns across social media and Google platforms. My work
+        combines storytelling, SEO, and visual production to deliver strategies
+        that connect with audiences and drive real results
+      </p>
 
+      <div className="grid grid-cols-4 gap-6 my-10">
 
-export default Services
+      </div>
+    </div>
+  );
+};
+
+export default Services;
