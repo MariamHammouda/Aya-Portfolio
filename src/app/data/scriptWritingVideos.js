a83455a46fa033257b08ce78eeeab3667fb83084
Mariam Hammouda <mariamhamouda24@gmail.com>
2025-10-04 12:26:10 +0300

    fill data

--- src/app/data/scriptWritingVideos.js
+++ src/app/data/scriptWritingVideos.js
@@ -92,6 +92,79 @@ export const scriptWritingPrograms = [
         ]
       }
     ]
+  },
+  {
+    id: 3,
+    programTitle: "عيادة الدكتوره ايريني سمير سامي",
+    programDescription: "محتوى طبي متخصص لعيادة الدكتوره ايريني سمير سامي يركز على التوعية الطبية والخدمات الصحية المتميزة",
+    programThumbnail: "/images/third-program/program-cover.png",
+    totalEpisodes: 4,
+    episodes: [
+      {
+        id: 1,
+        episodeNumber: 1,
+        title: "الحلقة الأولى - خدمات العيادة",
+        description: "تعريف بخدمات عيادة الدكتوره ايريني سمير سامي والتخصصات المتاحة",
+        thumbnail: "/images/third-program/program-cover.png",
+        videoType: "facebook_embedded",
+        videoUrl: "https://www.facebook.com/reel/1464275021330841/",
+        facebookUrl: "https://www.facebook.com/reel/1464275021330841/",
+        embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1464275021330841%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+        scriptHighlights: [
+          "تعريف بخدمات العيادة",
+          "التخصصات الطبية المتاحة",
+          "جودة الرعاية الصحية"
+        ]
+      },
+      {
+        id: 2,
+        episodeNumber: 2,
+        title: "الحلقة الثانية - الاستشارات الطبية",
+        description: "أهمية الاستشارات الطبية المتخصصة والفحوصات الدورية",
+        thumbnail: "/images/third-program/program-cover.png",
+        videoType: "facebook_embedded",
+        videoUrl: "https://www.facebook.com/reel/634647989118033/",
+        facebookUrl: "https://www.facebook.com/reel/634647989118033/",
+        embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F634647989118033%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+        scriptHighlights: [
+          "أهمية الاستشارات المتخصصة",
+          "الفحوصات الدورية",
+          "الوقاية والعلاج المبكر"
+        ]
+      },
+      {
+        id: 3,
+        episodeNumber: 3,
+        title: "الحلقة الثالثة - التقنيات الحديثة",
+        description: "استخدام أحدث التقنيات الطبية في التشخيص والعلاج",
+        thumbnail: "/images/third-program/program-cover.png",
+        videoType: "facebook_embedded",
+        videoUrl: "https://www.facebook.com/reel/2196511024120233/",
+        facebookUrl: "https://www.facebook.com/reel/2196511024120233/",
+        embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F2196511024120233%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+        scriptHighlights: [
+          "التقنيات الطبية الحديثة",
+          "دقة التشخيص",
+          "فعالية العلاج"
+        ]
+      },
+      {
+        id: 4,
+        episodeNumber: 4,
+        title: "الحلقة الرابعة - رعاية المرضى",
+        description: "الاهتمام برعاية المرضى وتقديم أفضل الخدمات الطبية",
+        thumbnail: "/images/third-program/program-cover.png",
+        videoType: "facebook_embedded",
+        videoUrl: "https://www.facebook.com/reel/1272306127802956/",
+        facebookUrl: "https://www.facebook.com/reel/1272306127802956/",
+        embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1272306127802956%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+        scriptHighlights: [
+          "رعاية شاملة للمرضى",
+          "خدمة طبية متميزة",
+          "راحة وأمان المرضى"
+        ]
+      }
+    ]
   }
 ];
 
